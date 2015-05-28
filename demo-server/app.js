80147ba16ca0036a2e3db020db314253404bd981
Bill Stavroulakis <bstavroulakis@gmail.com>
2015-05-28 01:20:24 -0700

    signed cookies

--- demo-server/app.js
+++ demo-server/app.js
@@ -1,13 +1,19 @@
 var jsonServer = require('../node_modules/json-server')
 var server = jsonServer.create() // Returns an Express server
 var router = jsonServer.router('demo-server/db.json') // Returns an Express router
+var express = require("../node_modules/express");
+var cookieParser = require('../node_modules/cookie-parser');
+var session = require('../node_modules/express-session');
+var path    = require("path");
+var app = express();
+server.use(cookieParser("secret", {"path": "/"}));
 server.get('/auth/login', function(req, res){
     var users = router.db.object.users;
     var username = req.query.username;var password = req.query.password;
     res.setHeader('Content-Type', 'application/json');
     for(var i=0;i<=users.length -1;i++){
         if(users[i].username == username && users[i].password == password){
-            res.cookie('usersession',"user", { maxAge: 900000, httpOnly: false });
+            res.cookie('usersession',users[i].id, { maxAge: 900000, httpOnly: false, signed:true });
             res.send(JSON.stringify({ success: true }));
         }else{
             res.send(JSON.stringify({ success: false }));
@@ -17,20 +23,13 @@ server.get('/auth/login', function(req, res){
 server.use(jsonServer.defaults) // logger, static and cors middlewares
 server.use(router) // Mount router on '/'
 server.listen(5000);
-
-var express = require("../node_modules/express");
-var cookieParser = require('../node_modules/cookie-parser');
-var session = require('../node_modules/express-session');
-var path    = require("path");
-
-var app = express();
-app.use(cookieParser());
+app.use(cookieParser("secret", {"path": "/"}));
 app.use(session({ secret: 'polymer dashboard secret', resave: false, saveUninitialized: true, cookie: { maxAge: 60000 }}));
-
 app.get('/', function(req, res){
-    if (!req.cookies.usersession) {
+    if (!req.signedCookies.usersession) {
         res.redirect('/pages/auth/auth.html');
     }else{
+        console.log(req.signedCookies.usersession)
         res.sendFile(path.join(__dirname+'\\..\\index.html'));
     }
 });
