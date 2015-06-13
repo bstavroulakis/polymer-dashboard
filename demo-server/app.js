e27ee87eea70ad88f598848a12cc23e75d619a1e
Bill Stavroulakis <bstavroulakis@gmail.com>
2015-06-12 22:57:17 -0700

    message updates

--- demo-server/app.js
+++ demo-server/app.js
@@ -6,7 +6,6 @@ var server       = jsonServer.create() // Returns an Express server
 var router       = jsonServer.router('demo-server/db.json') // Returns an Express router
 var path         = require("path");
 var app          = express();
-
 server.use(cookieParser("secret", {"path": "/"}));
 server.use(function(req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");res.setHeader("Access-Control-Allow-Credentials", "true");
@@ -37,7 +36,7 @@ server.get('/profile', function(req,res){
         }
     }
     res.send();
-})
+});
 server.use(jsonServer.defaults) // logger, static and cors middlewares
 server.use(router) // Mount router on '/'
 server.listen(5000);
@@ -57,11 +56,9 @@ app.get('/pages/auth/auth.html', function(req, res){
     res.sendFile(path.join(__dirname+'\\..\\pages/auth/auth.html'));
 });
 app.use(express.static(path.join(__dirname, '..\\')));
-
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 http.listen(8080);
-
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
