8772a3ae23b48a699f7aecc7126cc10ed8d43388
Bill Stavroulakis <bstavroulakis@gmail.com>
2015-07-14 02:40:09 +0300

    vulcanization

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var filters     = require('gulp-filter');
 var insert      = require('gulp-insert');
 var sass        = require('gulp-sass');
 var merge       = require('merge-stream');
+var vulcanize   = require('gulp-vulcanize');
+var $           = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
 
 var filter = filters(['*', '!app/css/skins/color-vars-template.scss', '!app/css/skins/skin-template.scss']);
 
@@ -30,7 +33,32 @@ gulp.task('copy', function () {
         'bower_components/**/*'
     ]).pipe(gulp.dest('app/bower_components'));
 
+    var vulcanized = gulp.src(['app/components/pd-dashboard/pd-dashboard.html'])
+        .pipe($.rename('pd-dashboard.vulcanized.html'))
+        .pipe(gulp.dest('app/components/pd-dashboard'));
+
     return merge(bower);
 });
 
-gulp.task('default', ['sass','insert', 'copy']);
+gulp.task('vulcanize', function () {
+    return gulp.src('app/components/pd-dashboard/pd-dashboard.vulcanized.html')
+        .pipe(vulcanize({
+            abspath: '',
+            excludes: ['bower_components/polymer/polymer.html',
+                'app/bower_components/polymer/polymer.html'
+            ],
+            stripExcludes: false
+        }))
+        .pipe(gulp.dest('app/components/pd-dashboard'));
+});
+
+gulp.task('precache', function (callback) {
+
+});
+
+gulp.task('default',  function (cb) {
+    runSequence(
+        ['sass','insert', 'copy'],
+        'vulcanize', 'precache',
+        cb)
+    });
